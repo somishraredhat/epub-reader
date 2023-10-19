2ca1003809d395347df454313d04055e4650bbfb
Sourabh Mishra <somishra@redhat.com>
2023-10-19 18:49:53 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import { ReactReader } from 'react-reader'
+
+const App = () => {
+  const queryParameters = new URLSearchParams(window.location.search)
+  const fileName = `files/alice.epub`
+  const [location, setLocation] = useState<string | number>(2)
+  return (
+    <div style={{ height: '100vh' }}>
+      <ReactReader
+        url={fileName}
+        location={location}
+        locationChanged={(epubcifi: string) => setLocation(epubcifi)}
+      />
+    </div>
+  )
+}
+
+export default App
