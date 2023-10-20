a068a4b58a115ed6fa4675ef8ee59176ffa2584c
Sourabh Mishra <somishra@redhat.com>
2023-10-20 12:07:20 +0530

    commit build folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import { ReactReader } from 'react-reader'
 
 const App = () => {
+  const queryParameters = new URLSearchParams(window.location.search)
+  const fileName = `files/${queryParameters.get("file")}`
   const [location, setLocation] = useState<string | number>(2)
   return (
     <div style={{ height: '100vh' }}>
       <ReactReader
-        url="/files/alice.epub"
+        url={fileName}
         location={location}
         locationChanged={(epubcifi: string) => setLocation(epubcifi)}
       />
